09b03d2b3ca890b72868a5762ac6bc7c6a84a084
João D. Batista <joao42lbatista@gmail.com>
2021-06-29 09:49:59 -0300

    05prog.py

---
+++ progs/05prog.py
@@ -0,0 +1,31 @@
+
+a = 10
+b = 3
+
+print("\nPython tem Exponeciação e espaço depois de sring e variável (print(\"a:\", a) -> a: 10)")
+
+print("\nValor de a:", a)
+print("Valor de b:", b)
+
+print("\nSoma a + b:", a + b)
+print("Subtração a - b:", a - b)
+print("Divisão a / b:", a / b)
+print("Multiplicação a * b:", a * b)
+print("Exponeciação a ** b:", a ** b)
+
+print("\nDivisão inteira a // b:", a // b)
+print("Resto da divisão inteira a % b: ", a % b)
+
+print("\nCuidado com valores float, são aproximações finitas dos números reais")
+print("Erros de aredondamento podem se acumular após sucessivas operações nesses valores")
+
+print("\nResultado extado das operações abaixo é 1, mas...")
+print("(100/3 - 33) * 3:", (100/3 - 33) * 3)
+print("100 - (33 * 3):", 100 - (33 * 3))
+
+print("\n8/4:", 8/4)
+print("8/3:", 8/3)
+
+print("\nint + int = int")
+print("float + float = float")
+print("int + float = float\n")
