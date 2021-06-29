c2f1d8109a58d06e7e2e7979aeac8b9ff03d047e
João D. Batista <joao42lbatista@gmail.com>
2021-06-29 09:50:19 -0300

    06prog.py

---
+++ progs/06prog.py
@@ -0,0 +1,22 @@
+
+a = 10
+b = 3
+
+print("\nTeste de comparadores")
+
+print("\nValor de a:", a)
+print("Valor de b:", b)
+
+print("\na == 11:", a == 11)
+print("b <= a:", b <= a)
+
+print("x = a > b")
+x = a > b
+print("print(x)", x)
+print("type(x)", type(x))
+print("b ** 2 > a", b ** 2 > a)
+
+print("\nOperadores lógicos (para ser usado com valores lógicos): and, or, not")
+print("b > a or b > 0:", b > a or b > 0)
+print("b > a and b > 0:", b > a and b > 0)
+print("not(a > b):", not (a > b))
