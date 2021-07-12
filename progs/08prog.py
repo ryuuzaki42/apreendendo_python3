08a9e993ae5ed8023d53c13d974d2837515f7dab
João D. Batista <joao42lbatista@gmail.com>
2021-07-12 16:01:56 -0300

    08prog.py

---
+++ progs/08prog.py
@@ -0,0 +1,20 @@
+
+import math
+
+print("\nmath.e:", math.e)
+
+print("\nmath.pi:", math.pi)
+
+print("\nmath.sqrt(2):", math.sqrt(2))
+
+# Importe de parte do módulo
+from math import pi
+print("\npi:", pi)
+
+# dir() retonr a lista de todos os símbolos da tabela do módulo atual
+print("\nprint(dir())")
+print(dir())
+
+print("\nprint(dir(math))")
+# dir(modulo) lista todas partes de um módulo
+print(dir(math))
