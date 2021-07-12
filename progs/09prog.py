02703b88d4d6519566d3bc8d3f5989b696831e28
João D. Batista <joao42lbatista@gmail.com>
2021-07-12 16:02:02 -0300

    09prog.py

---
+++ progs/09prog.py
@@ -0,0 +1,16 @@
+
+num = int(input("Digite um valor: "))
+
+if num % 2 == 0 :
+    print("Valor", num, "é par")
+else :
+    print("Valor", num, "é impar")
+
+nota = int(input("Digite o valor da nota: "))
+
+if nota >= 30 and nota < 60 :
+    print("Faz prova sub")
+else :
+    print("Não faz prova sub")
+
+print("Fim do programa")
