1623e944fa0d6d746b5221f9ae7975827cbdb33e
João D. Batista <joao42lbatista@gmail.com>
2021-07-15 17:28:25 -0300

    12prog.py

---
+++ progs/12prog.py
@@ -0,0 +1,9 @@
+
+val = int(input("Digite um valor: "))
+
+if val == 0:
+    print("val", val, "é igual a zero")
+elif val > 0:
+    print("val", val, "é positivo")
+else: # val < 0
+    print("val", val, "é negativo")
